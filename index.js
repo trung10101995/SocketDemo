449e2329cd46935cf7004750d6636e5bb51d4cbd
TrungCrit <Trung10101995@gmail.com>
2021-05-19 13:35:36 +0700

    ad

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const server = require('http').createServer();
+// const app = require('express')();
+// const server = require('http').createServer(app);
+// const io = require('socket.io')(server);
+// io.on('connection', () => { /* … */
+//   Console.log(client)
+//   });
+// server.listen(3010);
+
+
+const app = require('express')();
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-io.on('connection', client => {
-  client.on('event', data => { /* … */ });
-  client.on('disconnect', () => { /* … */ });
-  console.log(client.id)
-});
-server.listen(3123);
+io.on('connection', function(client){
+    console.log("socket dang lang nghe bạn");
+    client.on('event', function(data){});
+    client.on('disconnect', function(){});
+  });
+ server.listen(1999, function(){
+    console.log("start server in port 1999 ...");
+
+  });
