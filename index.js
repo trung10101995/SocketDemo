9f3df9409f35127c2ee2e0d9d4a5b474e02e9b82
TrungCrit <Trung10101995@gmail.com>
2021-05-19 16:04:59 +0700

    fix

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-
-
-  var express = require('express');
+ var express = require('express');
   var app = express();
   const server = require('http').createServer(app);
 
-  server.listen(2111,
-      function () {
-          console.log('listening on *: 2111 soket io');s
+  server.listen(2111, function () {
+          console.log('listening on *: 2111 soket io');
       });
   //  Console.log("start soketio")
   
