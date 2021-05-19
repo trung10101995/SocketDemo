919f3e64debe1279c6fa5edefe9959a165e3ae25
TrungCrit <Trung10101995@gmail.com>
2021-05-19 14:00:56 +0700

    fix

--- index.js
+++ index.js
@@ -7,15 +7,31 @@
 // server.listen(3010);
 
 
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-io.on('connection', function(client){
-    console.log("socket dang lang nghe bạn");
-    client.on('event', function(data){});
-    client.on('disconnect', function(){});
-  });
- server.listen(1999, function(){
-    console.log("start server in port 1999 ...");
+// const express = require('express');
+// var app = express()
+// const server = require('http').createServer(app);
+// const io = require('socket.io')(server);
+// io.on('connection', function(client){
+//     console.log("socket dang lang nghe bạn");
+//     client.on('event', function(data){});
+//     client.on('disconnect', function(){});
+//   });
+//  server.listen(1999, function(){
+//     console.log("start server in port 1999 ...");
+//
+//   });
 
+  var express = require('express');
+  var app = express();
+  const server = require('http').createServer(app);
+  const io = require('socket.io')(server);
+  io.on('connection', client => {
+    client.on('event', function(data){} );
+    client.on('disconnect', function(){});
+    console.log("có người kết nối")
   });
+  server.listen(1999,
+      function () {
+          console.log('listening on *: 1999 soket io');
+      });
+  //  Console.log("start soketio")
