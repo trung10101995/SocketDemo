4ef3eb3537636b7b995749df0527a60be53b0185
TrungCrit <Trung10101995@gmail.com>
2021-05-19 16:01:46 +0700

    fix

--- index.js
+++ index.js
@@ -1,37 +1,20 @@
-// const app = require('express')();
-// const server = require('http').createServer(app);
-// const io = require('socket.io')(server);
-// io.on('connection', () => { /* … */
-//   Console.log(client)
-//   });
-// server.listen(3010);
 
-
-// const express = require('express');
-// var app = express()
-// const server = require('http').createServer(app);
-// const io = require('socket.io')(server);
-// io.on('connection', function(client){
-//     console.log("socket dang lang nghe bạn");
-//     client.on('event', function(data){});
-//     client.on('disconnect', function(){});
-//   });
-//  server.listen(1999, function(){
-//     console.log("start server in port 1999 ...");
-//
-//   });
 
   var express = require('express');
   var app = express();
   const server = require('http').createServer(app);
-  const io = require('socket.io')(server);
-  io.on('connection', client => {
-    client.on('event', function(data){} );
-    client.on('disconnect', function(){});
-    console.log("có người kết nối")
-  });
-  server.listen(1999,
+
+  server.listen(2111,
       function () {
-          console.log('listening on *: 1999 soket io');
+          console.log('listening on *: 2111 soket io');s
       });
   //  Console.log("start soketio")
+  
+  app.get('/getDataSearch',function(req,res){
+
+    console.log("trung lòng 1")
+    res.send(JSON.stringify({
+        code: "200",
+        
+    }));
+});
